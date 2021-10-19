27dbe2fd497cdd445377de6d551034ea8607ae73
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-10-19 15:41:02 +0200

    Add more information to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@
 """The setup script."""
 from setuptools import find_packages, setup
 
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 install_requires = [
     "antspyx==0.2.4",
     "atldld==0.2.2",
@@ -58,6 +61,20 @@ setup(
         "write_to_template": '"""The package version."""\n__version__ = "{version}"\n',
         "local_scheme": "no-local-version",
     },
+    description="Align and improve brain annotation atlases",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlueBrain/atlas-annotation",
+    project_urls={
+        "Source Code": "https://github.com/BlueBrain/atlas-annotation",
+        "Bug Tracker": "https://github.com/BlueBrain/atlas-annotation/issues",
+    },
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3.7",
+    ],
     package_dir={"": "src"},
     packages=find_packages("src"),
     python_requires="~=3.7.0",
