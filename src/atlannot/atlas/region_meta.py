32a728f58ae9c18139cf86274bddb1b65cc146bd
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-11-04 19:53:31 +0100

    Implement RegionMeta.collect_ancestors

--- src/atlannot/atlas/region_meta.py
+++ src/atlannot/atlas/region_meta.py
@@ -75,6 +75,59 @@ class RegionMeta:
             if region_level == level:
                 yield region_id
 
+    def collect_ancestors(self, leaf_ids, top_id=None, remove_background=True):
+        """Collect all region IDs between the leaf regions and the top region.
+
+        Leaf regions that don't descent from the given top region are ignored.
+        The collection is inclusive, i.e. both the leaf region IDs and the
+        top region ID will be collected along with all intermediate regions.
+
+        If the top region is not provided it will default to the background.
+        This will effectively collect all parents of all leaves up to the top
+        since the background is at the top of the hierarchy.
+
+        Parameters
+        ----------
+        leaf_ids : iterable of int
+            The IDs of the leaf regions
+        top_id : int
+            The ID of the top region up to which to collect the parents.
+        remove_background : bool
+            If True it will be guaranteed that the background region ID is
+            not included in the result.
+
+        Returns
+        -------
+        set
+            All leaf IDs, the top region ID, and all the intermediate region IDs
+            between the leaf and the top regions.
+        """
+        if top_id is None:
+            top_id = self.background_id
+
+        def descends_from_top_id(child_id):
+            """Check if the given ID is a descendant of top_id."""
+            if child_id == top_id:
+                return True
+            if child_id is None:
+                return False
+
+            return descends_from_top_id(self.parent_id[child_id])
+
+        # The actual work - collect all ancestors up to the top_id
+        ids = {top_id}
+        for id_ in set(leaf_ids):
+            if not descends_from_top_id(id_):
+                continue
+            while id_ != top_id:
+                ids.add(id_)
+                id_ = self.parent_id[id_]
+
+        if remove_background:
+            ids -= {self.background_id}
+
+        return ids
+
     def _parse_region_hierarchy(self, region, parent_id=None):
         """Parse and save a region and its children.
 
