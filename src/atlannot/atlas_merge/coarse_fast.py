f2c00d37e7592a90b6cda0947cc4fbd6f162d78e
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-11-04 18:23:06 +0100

    Add the coarse_fast module

---
+++ src/atlannot/atlas_merge/coarse_fast.py
@@ -0,0 +1,300 @@
+# Copyright 2021, Blue Brain Project, EPFL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The coarse merging of the annotation atlases."""
+import logging
+
+import numpy as np
+
+from atlannot.atlas.region_meta import RegionMeta
+from atlannot.atlas_merge.JSONread import RegionData
+
+logger = logging.getLogger(__name__)
+
+
+def replace_label(atlas, old_value, new_value):
+    atlas[atlas == old_value] = new_value
+
+
+def manual_relabel(ccfv2_corrected, ccfv3_corrected):
+    # Entorhinal area, lateral part
+    replace_label(ccfv2_corrected, 60, 28)  # L6b -> L6a
+    replace_label(ccfv2_corrected, 999, 20)  # L2/3 -> L2 # double check?
+    replace_label(ccfv2_corrected, 715, 20)  # L2a -> L2
+    replace_label(ccfv2_corrected, 764, 20)  # L2b -> L2
+    replace_label(ccfv2_corrected, 92, 139)  # L4 -> L5
+    replace_label(ccfv2_corrected, 312, 139)  # L4/5 -> L5
+
+    # Entorhinal area, medial part, dorsal zone
+    replace_label(ccfv2_corrected, 468, 543)  # L2a -> L2
+    replace_label(ccfv2_corrected, 508, 543)  # L2b -> L2
+    replace_label(ccfv2_corrected, 712, 727)  # L4 -> L5 # double check?
+
+    replace_label(ccfv2_corrected, 195, 304)  # L2 -> L2/3
+    replace_label(ccfv2_corrected, 524, 582)  # L2 -> L2/3
+    replace_label(ccfv2_corrected, 606, 430)  # L2 -> L2/3
+    replace_label(ccfv2_corrected, 747, 556)  # L2 -> L2/3
+
+    # subreg of Cochlear nuclei -> Cochlear nuclei
+    replace_label(ccfv2_corrected, 96, 607)
+    replace_label(ccfv2_corrected, 101, 607)
+    replace_label(ccfv2_corrected, 112, 607)
+    replace_label(ccfv2_corrected, 560, 607)
+    replace_label(ccfv3_corrected, 96, 607)
+    replace_label(ccfv3_corrected, 101, 607)
+    # subreg of Nucleus ambiguus -> Nucleus ambiguus
+    replace_label(ccfv2_corrected, 143, 135)
+    replace_label(ccfv2_corrected, 939, 135)
+    replace_label(ccfv3_corrected, 143, 135)
+    replace_label(ccfv3_corrected, 939, 135)
+    # subreg of Accessory olfactory bulb -> Accessory olfactory bulb
+    replace_label(ccfv2_corrected, 188, 151)
+    replace_label(ccfv2_corrected, 196, 151)
+    replace_label(ccfv2_corrected, 204, 151)
+    replace_label(ccfv3_corrected, 188, 151)
+    replace_label(ccfv3_corrected, 196, 151)
+    replace_label(ccfv3_corrected, 204, 151)
+    # subreg of Medial mammillary nucleus -> Medial mammillary nucleus
+    replace_label(ccfv2_corrected, 798, 491)
+    replace_label(ccfv3_corrected, 798, 491)
+    replace_label(ccfv3_corrected, 606826647, 491)
+    replace_label(ccfv3_corrected, 606826651, 491)
+    replace_label(ccfv3_corrected, 606826655, 491)
+    replace_label(ccfv3_corrected, 606826659, 491)
+    # Subreg to Dorsal part of the lateral geniculate complex
+    replace_label(ccfv3_corrected, 496345664, 170)
+    replace_label(ccfv3_corrected, 496345668, 170)
+    replace_label(ccfv3_corrected, 496345672, 170)
+    # Subreg to Lateral reticular nucleus
+    replace_label(ccfv2_corrected, 955, 235)
+    replace_label(ccfv2_corrected, 963, 235)
+    replace_label(ccfv3_corrected, 955, 235)
+    replace_label(ccfv3_corrected, 963, 235)
+
+    # subreg of Posterior parietal association areas combined layer by layer
+    replace_label(ccfv3_corrected, 312782550, 532)
+    replace_label(ccfv3_corrected, 312782604, 532)
+    replace_label(ccfv3_corrected, 312782554, 241)
+    replace_label(ccfv3_corrected, 312782608, 241)
+    replace_label(ccfv3_corrected, 312782558, 635)
+    replace_label(ccfv3_corrected, 312782612, 635)
+    replace_label(ccfv3_corrected, 312782562, 683)
+    replace_label(ccfv3_corrected, 312782616, 683)
+    replace_label(ccfv3_corrected, 312782566, 308)
+    replace_label(ccfv3_corrected, 312782620, 308)
+    replace_label(ccfv3_corrected, 312782570, 340)
+    replace_label(ccfv3_corrected, 312782624, 340)
+
+    # subreg to Parabrachial nucleus
+    replace_label(ccfv2_corrected, 123, 867)
+    replace_label(ccfv2_corrected, 860, 867)
+    replace_label(ccfv2_corrected, 868, 867)
+    replace_label(ccfv2_corrected, 875, 867)
+    replace_label(ccfv2_corrected, 883, 867)
+    replace_label(ccfv2_corrected, 891, 867)
+    replace_label(ccfv2_corrected, 899, 867)
+    replace_label(ccfv2_corrected, 915, 867)
+    replace_label(ccfv3_corrected, 123, 867)
+
+
+def merge(ccfv2, ccfv3, brain_regions):
+    """Perform the coarse atlas merging.
+
+    Parameters
+    ----------
+    ccfv2 : np.ndarray
+        The first atlas to merge, usually CCFv2.
+    ccfv3 : np.ndarray
+        The second atlas to merge, usually CCFv3.
+    brain_regions : dict
+        The brain regions dictionary. Can be obtained from the "msg" key of
+        the `brain_regions.json` (`1.json`) file.
+
+    Returns
+    -------
+    ccfv2_corrected : np.ndarray
+        The merged CCFv2 atlas.
+    ccfv3_corrected : np.ndarray
+        The merged CCFv3 atlas.
+    """
+    region_data = RegionData(brain_regions)
+
+    logger.info("Computing uniques")
+    uniques_v2 = region_data.find_unique_regions(ccfv2, top_region_name="root")
+    uniques_v3 = region_data.find_unique_regions(ccfv3, top_region_name="root")
+
+    logger.info("Computing children")
+    children_v2, _ = region_data.find_children(uniques_v2)
+    children_v3, _ = region_data.find_children(uniques_v3)
+
+    logger.info("Prepping new arrays")
+    ccfv2_corrected = ccfv2.copy()
+    ccfv3_corrected = ccfv3.copy()
+    ids = np.unique(ccfv2_corrected)
+    ids2 = np.unique(ccfv3_corrected)
+
+    logger.info("First loop")
+    ids_to_correct = ids[np.isin(ids, ids2, invert=True)]
+    for id_reg in ids_to_correct:
+        allname = region_data.id_to_region_dictionary_ALLNAME[id_reg]
+        if (
+            region_data.is_leaf[allname]
+            and id_reg not in ids2
+            and region_data.region_dictionary_to_id[
+                region_data.region_dictionary_to_id_parent[
+                    region_data.id_to_region_dictionary[id_reg]
+                ]
+            ]
+            in ids2
+        ):
+            ccfv2_corrected[
+                ccfv2_corrected == id_reg
+            ] = region_data.region_dictionary_to_id[
+                region_data.region_dictionary_to_id_parent[
+                    region_data.id_to_region_dictionary[id_reg]
+                ]
+            ]
+        elif region_data.is_leaf[allname] and (
+            "Medial amygdalar nucleus" in allname
+            or "Subiculum" in allname
+            or "Bed nuclei of the stria terminalis" in allname
+        ):
+            ccfv2_corrected[
+                ccfv2_corrected == id_reg
+            ] = region_data.region_dictionary_to_id[
+                region_data.region_dictionary_to_id_parent[
+                    region_data.region_dictionary_to_id_parent[
+                        region_data.id_to_region_dictionary[id_reg]
+                    ]
+                ]
+            ]
+        elif "Paraventricular hypothalamic nucleus" in allname:
+            ccfv2_corrected[ccfv2_corrected == id_reg] = 38
+
+    logger.info("Manual replacements")
+    manual_relabel(ccfv2_corrected, ccfv3_corrected)
+
+    logger.info("Second loop")
+    for id_reg in np.unique(np.concatenate((ids, ids2)))[1:]:
+        allname = region_data.id_to_region_dictionary_ALLNAME[id_reg]
+        if "Visual areas" in allname:
+            if "ayer 1" in allname:
+                ccfv3_corrected[np.where(ccfv3_corrected == id_reg)] = 801
+                ccfv2_corrected[np.where(ccfv2_corrected == id_reg)] = 801
+            elif "ayer 2/3" in allname:
+                ccfv3_corrected[np.where(ccfv3_corrected == id_reg)] = 561
+                ccfv2_corrected[np.where(ccfv2_corrected == id_reg)] = 561
+            elif "ayer 4" in allname:
+                ccfv3_corrected[np.where(ccfv3_corrected == id_reg)] = 913
+                ccfv2_corrected[np.where(ccfv2_corrected == id_reg)] = 913
+            elif "ayer 5" in allname:
+                ccfv3_corrected[np.where(ccfv3_corrected == id_reg)] = 937
+                ccfv2_corrected[np.where(ccfv2_corrected == id_reg)] = 937
+            elif "ayer 6a" in allname:
+                ccfv3_corrected[np.where(ccfv3_corrected == id_reg)] = 457
+                ccfv2_corrected[np.where(ccfv2_corrected == id_reg)] = 457
+            elif "ayer 6b" in allname:
+                ccfv3_corrected[np.where(ccfv3_corrected == id_reg)] = 497
+                ccfv2_corrected[np.where(ccfv2_corrected == id_reg)] = 497
+
+    logger.info("Manual replacements #2")
+    # subreg of Prosubiculum to subiculum
+    ccfv3_corrected[np.where(ccfv3_corrected == 484682470)] = 502
+    # Orbital area, medial part, layer 6b -> 6a
+    ccfv3_corrected[np.where(ccfv3_corrected == 527696977)] = 910
+    ccfv3_corrected[np.where(ccfv3_corrected == 355)] = 314
+
+    logger.info("Third loop")
+    for id_reg in ids2[1:]:
+        if (
+            (
+                "fiber tracts" in region_data.id_to_region_dictionary_ALLNAME[id_reg]
+                or "Interpeduncular nucleus"
+                in region_data.id_to_region_dictionary_ALLNAME[id_reg]
+            )
+            and id_reg not in ids
+            and region_data.region_dictionary_to_id[
+                region_data.region_dictionary_to_id_parent[
+                    region_data.id_to_region_dictionary[id_reg]
+                ]
+            ]
+            in ids
+        ):
+            ccfv3_corrected[
+                np.where(ccfv3_corrected == id_reg)
+            ] = region_data.region_dictionary_to_id[
+                region_data.region_dictionary_to_id_parent[
+                    region_data.id_to_region_dictionary[id_reg]
+                ]
+            ]
+        if (
+            "Frontal pole, cerebral cortex"
+            in region_data.id_to_region_dictionary_ALLNAME[id_reg]
+        ):
+            ccfv3_corrected[np.where(ccfv3_corrected == id_reg)] = 184
+            ccfv2_corrected[np.where(ccfv2_corrected == id_reg)] = 184
+
+    logger.info("Some manual stuff again")
+    ids = np.unique(ccfv2_corrected)
+    ids2 = np.unique(ccfv3_corrected)
+    ids_to_correct = ids[np.isin(ids, ids2, invert=True)]
+
+    ids_to_correct = ids2[np.isin(ids2, ids, invert=True)]
+
+    ids_to_correct = ids_to_correct[ids_to_correct != 997]
+    ids_to_correct = ids_to_correct[ids_to_correct != 8]
+
+    logger.info("While loop 1")
+    while len(ids_to_correct) > 0:
+        parent = ids_to_correct[0]
+        while parent not in uniques_v2:
+            parent = region_data.region_dictionary_to_id[
+                region_data.region_dictionary_to_id_parent[
+                    region_data.id_to_region_dictionary[parent]
+                ]
+            ]
+        for child in children_v3[region_data.id_to_region_dictionary_ALLNAME[parent]]:
+            ccfv3_corrected[np.where(ccfv3_corrected == child)] = parent
+        for child in children_v2[region_data.id_to_region_dictionary_ALLNAME[parent]]:
+            ccfv2_corrected[np.where(ccfv2_corrected == child)] = parent
+        ids = np.unique(ccfv2_corrected)
+        ids2 = np.unique(ccfv3_corrected)
+        ids_to_correct = ids2[np.isin(ids2, ids, invert=True)]
+        ids_to_correct = ids_to_correct[ids_to_correct != 997]
+        ids_to_correct = ids_to_correct[ids_to_correct != 8]
+
+    logger.info("Post while loop 1")
+    ids_to_correct = ids[np.isin(ids, ids2, invert=True)]
+    ids_to_correct = ids_to_correct[ids_to_correct != 997]
+    ids_to_correct = ids_to_correct[ids_to_correct != 8]
+
+    logger.info("While loop 2")
+    while len(ids_to_correct) > 0:
+        parent = ids_to_correct[0]
+        while parent not in uniques_v3:
+            parent = region_data.region_dictionary_to_id[
+                region_data.region_dictionary_to_id_parent[
+                    region_data.id_to_region_dictionary[parent]
+                ]
+            ]
+        for child in children_v3[region_data.id_to_region_dictionary_ALLNAME[parent]]:
+            ccfv3_corrected[np.where(ccfv3_corrected == child)] = parent
+        for child in children_v2[region_data.id_to_region_dictionary_ALLNAME[parent]]:
+            ccfv2_corrected[np.where(ccfv2_corrected == child)] = parent
+        ids = np.unique(ccfv2_corrected)
+        ids2 = np.unique(ccfv3_corrected)
+        ids_to_correct = ids[np.isin(ids, ids2, invert=True)]
+        ids_to_correct = ids_to_correct[ids_to_correct != 997]
+        ids_to_correct = ids_to_correct[ids_to_correct != 8]
+
+    return ccfv2_corrected, ccfv3_corrected
