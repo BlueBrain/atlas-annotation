1d8abfa092bc979db784093e2580582d3eb11d63
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-11-05 18:28:11 +0100

    Eliminate filter_region

--- src/atlannot/atlas_merge/fine_fast.py
+++ src/atlannot/atlas_merge/fine_fast.py
@@ -278,34 +278,6 @@ def merge(ccfv2, ccfv3, brain_regions):
 
         return all_descendants
 
-    def filter_region(annotation, id_reg, descendant_ids):
-        """Filter a region.
-
-        Computes a 3d boolean mask to filter a region and its subregion from
-        the annotations.
-
-        Parameters
-        ----------
-        annotation : np.ndarray
-            3D numpy ndarray of integers ids of the regions.
-        id_reg : int
-            The region ID.
-        descendant_ids : set
-            List of descendand IDs of the given region
-
-        Returns
-        -------
-        filter_ : np.ndarray
-            3D numpy ndarray of boolean, boolean mask with all the voxels of a
-            region and its children set to True.
-        """
-        if is_leaf(id_reg):
-            filter_ = annotation == id_reg
-            assert len(descendant_ids) == 0
-        else:
-            filter_ = np.isin(annotation, [id_reg, *descendant_ids])
-        return filter_
-
     logger.info("First for-loop correction")
     unique_v2 = set(v2_to)
     unique_v3 = set(v3_to)
@@ -378,11 +350,7 @@ def merge(ccfv2, ccfv3, brain_regions):
     # Correct annotation edge for ccfv2 and ccfv3
     # no limit for striatum
     id_reg = 278
-    filter_ = filter_region(
-        ccfv2_corrected,
-        id_reg,
-        descendants(id_reg, allowed_v2),
-    )
+    filter_ = np.isin(ccfv2_corrected, [id_reg, *descendants(id_reg, allowed_v2)])
     copy_filt = np.copy(ccfv2_corrected)
     copy_filt[~filter_] = 0
     error_voxel = np.where(copy_filt == id_reg)
@@ -390,11 +358,7 @@ def merge(ccfv2, ccfv3, brain_regions):
         ccfv2_corrected[x, y, z] = explore_voxel([x, y, z], copy_filt, -1)
 
     for id_reg in (803, 477):
-        filter_ = filter_region(
-            ccfv3_corrected,
-            id_reg,
-            descendants(id_reg, allowed_v2),
-        )
+        filter_ = np.isin(ccfv3_corrected, [id_reg, *descendants(id_reg, allowed_v2)])
         copy_filt = np.copy(ccfv3_corrected)
         copy_filt[~filter_] = 0
         error_voxel = np.where(copy_filt == id_reg)
@@ -404,11 +368,7 @@ def merge(ccfv2, ccfv3, brain_regions):
     # Correct ccfv2 annotation edge Cerebral cortex, Basic Cell group and
     # regions and root  1089, 688, 8, 997
     for id_reg in (688, 8, 997):
-        filter_ = filter_region(
-            ccfv2_corrected,
-            id_reg,
-            descendants(id_reg, allowed_v2),
-        )
+        filter_ = np.isin(ccfv2_corrected, [id_reg, *descendants(id_reg, allowed_v2)])
         copy_filt = np.copy(ccfv2_corrected)
         copy_filt[~filter_] = 0
         error_voxel = np.where(copy_filt == id_reg)
@@ -417,11 +377,7 @@ def merge(ccfv2, ccfv3, brain_regions):
 
     # Correct ccfv3 annotation edge for Hippocampal formation, Cortical subplate
     for id_reg in (1089, 703):
-        filter_ = filter_region(
-            ccfv3_corrected,
-            id_reg,
-            descendants(id_reg, allowed_v2),
-        )
+        filter_ = np.isin(ccfv3_corrected, [id_reg, *descendants(id_reg, allowed_v2)])
         copy_filt = np.copy(ccfv3_corrected)
         copy_filt[~filter_] = 0
         error_voxel = np.where(copy_filt == id_reg)
