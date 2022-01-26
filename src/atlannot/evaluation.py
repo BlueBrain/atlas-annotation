fd1471882b8dca5f6b35891970fe339c9f252516
Emilie Delattre <emilie.delattre@epfl.ch>
2022-01-26 10:56:36 +0100

    Replace atlas_remap by np.isin

--- src/atlannot/evaluation.py
+++ src/atlannot/evaluation.py
@@ -186,7 +186,7 @@ def evaluate(
         # Jaggedness
         values_from = np.unique(atlas)
         values_to = np.array([1 if value in desc else 0 for value in values_from])
-        mask = atlas_remap(atlas, values_from, values_to)
+        mask = np.isin(atlas, desc)
         global_jaggedness = compute_jaggedness(mask, region_ids=[1])[1]
         per_region_jaggedness = compute_jaggedness(atlas, region_ids=desc)
 
