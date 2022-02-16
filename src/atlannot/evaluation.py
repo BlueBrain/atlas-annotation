490979fe877b211e8c5efe37668ee002d6046f1e
Emilie Delattre <emilie.delattre@epfl.ch>
2022-02-16 15:42:00 +0100

    Change typing.Sequence for collections.abc.Sequence

--- src/atlannot/evaluation.py
+++ src/atlannot/evaluation.py
@@ -15,7 +15,8 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from typing import Any, Sequence
+from collections.abc import Sequence
+from typing import Any
 
 import numpy as np
 from atlalign.metrics import iou_score
