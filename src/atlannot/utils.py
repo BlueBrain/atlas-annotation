01b14855b4bce00cb615abd29cb4b9c19a328772
Jan <jankrepl@yahoo.com>
2022-06-01 10:09:57 +0200

    Copy paste all the code from elsewhere

--- src/atlannot/utils.py
+++ src/atlannot/utils.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Various utilities. (Refactoring in the near future)."""
+from __future__ import annotations
+
 import math
 import os
 import pathlib
@@ -452,3 +454,159 @@ def atlas_symmetry_score(atlas, axis=1):
     half_1, half_2 = split_halfs(atlas, axis)
     alignment = 1 - get_misalignment(half_1, half_2)
     return alignment
+
+
+class Remapper:
+    """Utility class for remapping of labels."""
+
+    def __init__(self, *volumes: list[np.ndarray]) -> None:
+        # initial checks
+        if not volumes:
+            raise ValueError("No volume provided")
+
+        for volume in volumes:
+            if not isinstance(volume, np.ndarray):
+                raise TypeError("The inputs need to be numpy arrays")
+            if not volume.dtype == np.uint32:
+                raise TypeError("The dtype of the arrays needs to be uint32")
+
+        self.storage: list[tuple[np.ndarray, np.ndarray, tuple[int, ...]]] = [
+            (*np.unique(volume, return_inverse=True), volume.shape)
+            for volume in volumes
+        ]
+        unique_overall = set()
+        for unique, _, _ in self.storage:
+            unique_overall |= set(unique)
+
+        self.old2new = {x: i for i, x in enumerate(sorted(unique_overall))}
+        self.new2old = {v: k for k, v in self.old2new.items()}
+
+    def __len__(self) -> int:
+        return len(self.storage)
+
+    @staticmethod
+    def remap(
+        shape: tuple[int, ...],
+        mapping: dict[int, int],
+        unique: np.ndarray,
+        inv: np.ndarray,
+    ) -> np.ndarray:
+        runique = np.array([mapping[x] for x in unique], dtype=np.uint32)
+
+        return runique[inv].reshape(shape)
+
+    def remap_old_to_new(self, i: int) -> np.ndarray:
+        if not (0 <= i < len(self)):
+            raise IndexError
+
+        unique, inv, shape = self.storage[i]
+        return self.remap(shape, self.old2new, unique, inv)
+
+    def remap_new_to_old(self, volume: np.ndarray) -> np.ndarray:
+        unique, inv = np.unique(volume, return_inverse=True)
+
+        return self.remap(volume.shape, self.new2old, unique, inv)
+
+
+class Remapper:
+    """Utility class for remapping of labels."""
+
+    def __init__(self, *volumes: list[np.ndarray]) -> None:
+        # initial checks
+        if not volumes:
+            raise ValueError("No volume provided")
+
+        for volume in volumes:
+            if not isinstance(volume, np.ndarray):
+                raise TypeError("The inputs need to be numpy arrays")
+            if not volume.dtype == np.uint32:
+                raise TypeError("The dtype of the arrays needs to be uint32")
+
+        self.storage: list[tuple[np.ndarray, np.ndarray, tuple[int, ...]]] = [
+            (*np.unique(volume, return_inverse=True), volume.shape)
+            for volume in volumes
+        ]
+        unique_overall = set()
+        for unique, _, _ in self.storage:
+            unique_overall |= set(unique)
+
+        self.old2new = {x: i for i, x in enumerate(sorted(unique_overall))}
+        self.new2old = {v: k for k, v in self.old2new.items()}
+
+    def __len__(self) -> int:
+        return len(self.storage)
+
+    @staticmethod
+    def remap(
+        shape: tuple[int, ...],
+        mapping: dict[int, int],
+        unique: np.ndarray,
+        inv: np.ndarray,
+    ) -> np.ndarray:
+        runique = np.array([mapping[x] for x in unique], dtype=np.uint32)
+
+        return runique[inv].reshape(shape)
+
+    def remap_old_to_new(self, i: int) -> np.ndarray:
+        if not (0 <= i < len(self)):
+            raise IndexError
+
+        unique, inv, shape = self.storage[i]
+        return self.remap(shape, self.old2new, unique, inv)
+
+    def remap_new_to_old(self, volume: np.ndarray) -> np.ndarray:
+        unique, inv = np.unique(volume, return_inverse=True)
+
+        return self.remap(volume.shape, self.new2old, unique, inv)
+
+
+class Remapper:
+    """Utility class for remapping of labels."""
+
+    def __init__(self, *volumes: list[np.ndarray]) -> None:
+        # initial checks
+        if not volumes:
+            raise ValueError("No volume provided")
+
+        for volume in volumes:
+            if not isinstance(volume, np.ndarray):
+                raise TypeError("The inputs need to be numpy arrays")
+            if not volume.dtype == np.uint32:
+                raise TypeError("The dtype of the arrays needs to be uint32")
+
+        self.storage: list[tuple[np.ndarray, np.ndarray, tuple[int, ...]]] = [
+            (*np.unique(volume, return_inverse=True), volume.shape)
+            for volume in volumes
+        ]
+        unique_overall = set()
+        for unique, _, _ in self.storage:
+            unique_overall |= set(unique)
+
+        self.old2new = {x: i for i, x in enumerate(sorted(unique_overall))}
+        self.new2old = {v: k for k, v in self.old2new.items()}
+
+    def __len__(self) -> int:
+        return len(self.storage)
+
+    @staticmethod
+    def remap(
+        shape: tuple[int, ...],
+        mapping: dict[int, int],
+        unique: np.ndarray,
+        inv: np.ndarray,
+    ) -> np.ndarray:
+        runique = np.array([mapping[x] for x in unique], dtype=np.uint32)
+
+        return runique[inv].reshape(shape)
+
+    def remap_old_to_new(self, i: int) -> np.ndarray:
+        if not (0 <= i < len(self)):
+            raise IndexError
+
+        unique, inv, shape = self.storage[i]
+        return self.remap(shape, self.old2new, unique, inv)
+
+    def remap_new_to_old(self, volume: np.ndarray) -> np.ndarray:
+        unique, inv = np.unique(volume, return_inverse=True)
+
+        return self.remap(volume.shape, self.new2old, unique, inv)
