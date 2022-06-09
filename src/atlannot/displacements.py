07b6cf594ed0ab9c9d1e780d7716ce471bd43527
Emilie Delattre <emilie.delattre@epfl.ch>
2022-06-09 14:59:22 +0200

    Fix linting

--- src/atlannot/displacements.py
+++ src/atlannot/displacements.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """The Atlas Annotation (atlannot) package."""
-import pathlib
-
 import numpy as np
 from atldld.base import DisplacementField
 
